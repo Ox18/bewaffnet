a90effb5377ba87a1d2f27d2091c712c5c99f4b0
Alex <Wilmer.DelgadoAlama09@gmail.com>
2022-06-01 22:03:59 -0500

    update from C to C++ by classes

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include "raylib.h"
+
+int main()
+{
+    InitWindow(800, 600, "Gunbound");
+
+    Vector2 ballPosition = { 400.0f,300.0f };
+    
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        
+        // clears the screen
+        ClearBackground(BLACK);
+
+        // get the mouse position
+        Vector2 mousePosition = GetMousePosition();
+
+        ballPosition = mousePosition;
+
+        DrawCircleV(ballPosition, 20.0f, BLUE);
+
+        EndDrawing();
+    }
+
+    CloseWindow();
+    
+    return 0;
+}
